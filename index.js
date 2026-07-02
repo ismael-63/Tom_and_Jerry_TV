59936f4a511ecdc23daab9ffbc7949259b9bb8e8
Ismael <islaral63@gmail.com>
2026-07-02 21:30:52 +0200

    fucnionalidades terminadas

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const VIDEO_URL = (location.href + "/db.php");
 
+let videosGlobal = [];
+let indiceActual = 0;
+
 
 //Funcion autoinvocada para obtener los videos
 (async () => {
     await crearLinks();
-    let videos = await getVideos();
+    videosGlobal = await getVideos();
     await mostrarVideoInicial();
+    funcionalidadBotones();
+    autoPlay();
 })();
 
 
@@ -26,19 +31,24 @@ async function crearLinks(){
     let videos = await getVideos();
     videos.forEach(video => {
         let link = document.createElement("a");
-        link.href = video.url;
+        link.href = "#";
         link.textContent = video.name;
         link.classList.add("ep_link");
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            let indice = videosGlobal.findIndex(v => v.url === video.url);
+            mostrarVideo(indice);
+        });
         eps.appendChild(link);
     });
 }
 
 async function mostrarVideoInicial(){
-    let video_element = document.querySelector(".video");
-    let videos = await getVideos();
-
-    videos = shuffleArray(videos);
-    video_element.src = videos[0].url;
+    let video_element = document.getElementById("video");
+    
+    let videosAleatorios = shuffleArray([...videosGlobal]);
+    indiceActual = videosGlobal.findIndex(v => v.url === videosAleatorios[0].url);
+    video_element.src = videosGlobal[indiceActual].url;
 }   
 
 function shuffleArray(array){ // Algoritmo Fisher-Yates
@@ -49,5 +59,53 @@ function shuffleArray(array){ // Algoritmo Fisher-Yates
     }
 
     return array;
+}
+
+function mostrarVideo(indice){
+    let video_element = document.getElementById("video");
+    
+    if(indice < 0) indice = videosGlobal.length - 1;
+    if(indice >= videosGlobal.length) indice = 0;
+    
+    indiceActual = indice;
+    video_element.src = videosGlobal[indiceActual].url;
+}
+
+function mostrarSiguiente(){
+    mostrarVideo(indiceActual + 1);
+}
+
+function mostrarAnterior(){
+    mostrarVideo(indiceActual - 1);
+}
+
+function mostrarAleatorio(){
+    let indiceAleatorio = Math.floor(Math.random() * videosGlobal.length);
+    mostrarVideo(indiceAleatorio);
+}
+
+function funcionalidadBotones(){
+    let botones = document.querySelectorAll(".video_controls button");
+    
+    botones[0].addEventListener("click", mostrarAnterior);
+    botones[1].addEventListener("click", mostrarAleatorio);
+    botones[2].addEventListener("click", mostrarSiguiente);
+}
+
+function autoPlay(){
+    let video_element = document.getElementById("video");
+    
+    video_element.addEventListener("ended", () => {
+        if(indiceActual === videosGlobal.length - 1){
+            // Si es el último video, aleatorizar y reiniciar
+            let videosAleatorios = shuffleArray([...videosGlobal]);
+            indiceActual = videosGlobal.findIndex(v => v.url === videosAleatorios[0].url);
+        } else {
+            // Si no, ir al siguiente
+            indiceActual++;
+        }
+        video_element.src = videosGlobal[indiceActual].url;
+        video_element.play();
+    });
 }
 
